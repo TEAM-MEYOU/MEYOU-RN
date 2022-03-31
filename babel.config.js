90cbfbce43e35c3c29d198482039604d1227ecb3
gudwh14 <alxious@naver.com>
2022-03-31 22:18:56 +0900

    style : 파일경로 절대경로로 변경

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '~': './src',
+          '@components': './src/components',
+          '@pages': './src/pages',
+          '@constants': './src/constants',
+          '@assets': './src/assets',
+          '@utils': './src/utils',
+          '@hooks': './src/hooks',
+          '@stores': './src/stores',
+        },
+      },
+    ],
+  ],
 };
