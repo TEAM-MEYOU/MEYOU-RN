da0ed1c6d1b7bf5dc3ee903b347533f7e6ac7384
gudwh14 <alxious@naver.com>
2022-04-29 17:11:31 +0900

    feat : Layout 컴포넌트 스타일 수정 (#5)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,6 +4,8 @@ import colors from '@constants/colors';
 const Layout = styled.View`
   width: 100%;
   height: 100%;
+  align-items: center;
+  padding: 10px 0;
   background-color: ${colors.background};
 `;
 
