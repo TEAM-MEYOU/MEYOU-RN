5d5d2b9fdc0b9bec453817f957f7546504ab7d7c
gudwh14 <alxious@naver.com>
2022-05-12 10:26:50 +0900

    feat : useFetchCouple Query 추가 (#21)

--- src/hooks/queries.tsx
+++ src/hooks/queries.tsx
@@ -3,6 +3,7 @@ import { checkMember, Member } from '@apis/member';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState } from 'react';
 import { AxiosError } from 'axios';
+import { Couple, getCouple } from '@apis/couple';
 
 export const useFetchUser = (options?: UseQueryOptions<Member, AxiosError, Member, 'user'>) => {
   const [kakao, setKakao] = useState('');
@@ -21,4 +22,16 @@ export const useFetchUser = (options?: UseQueryOptions<Member, AxiosError, Membe
   }, []);
 
   return user;
+};
+
+export const useFetchCouple = (
+  coupleId: number | undefined,
+  options?: UseQueryOptions<Couple, AxiosError, Couple, 'couple'>
+) => {
+  const couple: UseQueryResult<Couple, AxiosError> = useQuery('couple', () => getCouple(coupleId!), {
+    enabled: coupleId !== undefined,
+    ...options,
+  });
+
+  return couple;
 };
