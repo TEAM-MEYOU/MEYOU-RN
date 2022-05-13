5a9ccda7888a49ab149e0f588e2dd22810e275ac
gudwh14 <alxious@naver.com>
2022-05-13 15:20:26 +0900

    feat : MainNavigation Enhancement (#21)

--- src/navigation/MainNavigation.tsx
+++ src/navigation/MainNavigation.tsx
@@ -1,23 +1,94 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '@screens/HomeScreen';
 import DiaryScreen from '@screens/DiaryScreen';
 import ChartScreen from '@screens/ChartScreen';
 import GameScreen from '@screens/GameScreen';
 import OptionScreen from '@screens/OptionScreen';
+import { Image } from 'react-native';
+import colors from '@constants/colors';
 
-const MainTab = createBottomTabNavigator();
+export type MainTabNavigator = {
+  Home: undefined;
+  Diary: undefined;
+  Chart: undefined;
+  Game: undefined;
+  Option: undefined;
+};
+const MainTab = createBottomTabNavigator<MainTabNavigator>();
+
+const TabMenu = {
+  Home: 'home',
+  Diary: 'diary',
+  Chart: 'chart',
+  Game: 'game',
+  Option: 'option',
+} as const;
+
+type TabMenuValue = typeof TabMenu[keyof typeof TabMenu];
 
 function MainNavigation() {
   return (
-    <MainTab.Navigator initialRouteName={'Home'}>
-      <MainTab.Screen name={'Home'} component={HomeScreen} />
-      <MainTab.Screen name={'Diary'} component={DiaryScreen} />
-      <MainTab.Screen name={'Chart'} component={ChartScreen} />
-      <MainTab.Screen name={'Game'} component={GameScreen} />
-      <MainTab.Screen name={'Option'} component={OptionScreen} />
+    <MainTab.Navigator
+      initialRouteName={'Home'}
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarInactiveTintColor: colors.grey400,
+        tabBarActiveTintColor: colors.content200,
+      }}>
+      <MainTab.Screen
+        name={'Home'}
+        component={HomeScreen}
+        options={{ tabBarIcon: props => <TabBarIcon name={'home'} {...props} /> }}
+      />
+      <MainTab.Screen
+        name={'Diary'}
+        component={DiaryScreen}
+        options={{ tabBarIcon: props => <TabBarIcon name={'diary'} {...props} /> }}
+      />
+      <MainTab.Screen
+        name={'Chart'}
+        component={ChartScreen}
+        options={{ tabBarIcon: props => <TabBarIcon name={'chart'} {...props} /> }}
+      />
+      <MainTab.Screen
+        name={'Game'}
+        component={GameScreen}
+        options={{ tabBarIcon: props => <TabBarIcon name={'game'} {...props} /> }}
+      />
+      <MainTab.Screen
+        name={'Option'}
+        component={OptionScreen}
+        options={{ tabBarIcon: props => <TabBarIcon name={'option'} {...props} /> }}
+      />
     </MainTab.Navigator>
   );
 }
+
+interface Props {
+  focused: boolean;
+  color: string;
+  size: number;
+  name: TabMenuValue;
+}
+
+const TabBarIcon = (props: Props) => {
+  const getIconPath = useCallback(() => {
+    switch (props.name) {
+      case 'home':
+        return require('/assets/icons/home.png');
+      case 'chart':
+        return require('/assets/icons/chart.png');
+      case 'diary':
+        return require('/assets/icons/diary.png');
+      case 'game':
+        return require('/assets/icons/game.png');
+      case 'option':
+        return require('/assets/icons/setting.png');
+    }
+  }, []);
+  return <Image style={{ width: props.size, height: props.size, tintColor: props.color }} source={getIconPath()} />;
+};
 
 export default MainNavigation;
